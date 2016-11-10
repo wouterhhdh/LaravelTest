6ce429ef412e7a1a1837898334d5cb0b76e97071
wouterhhdh <wouter@heyheydehaas.com>
2016-11-10 15:08:42 +0100

    about heeft nu een footer
    layout heeft @yield(‘footer’)

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -5,4 +5,8 @@
 {{-- alles wat hier staat is dus 'content' en zal worden geplaatst binnen layout.blade.php op de plek waar 'content' wordt genoemd --}}
     <h1>About page</h1>
 @stop
- 
+
+@section('footer')
+    <script type="text/javascript"> console.log("hi")</script>
+    {{-- dit script komt wel op de about pagina maar niet op de blade pagina. --}}
+@stop
