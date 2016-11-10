cae7aae11f74d686f534ca08e24e42986b3a23e9
wouterhhdh <wouter@heyheydehaas.com>
2016-11-10 16:34:27 +0100

    set-up of cards/index page

---
+++ resources/views/cards/index.blade.php
@@ -0,0 +1,5 @@
+@extends('layout')
+
+@section('content')
+	<h1>Cards</h1>
+@stop
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@
 Route::get('/', 'PagesController@home');
 Route::get('about', 'PagesController@about');
 
+Route::get('cards', 'CardsController@index');
+
+
