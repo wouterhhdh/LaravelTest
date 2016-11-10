0273c018b9b661bed9c877aecd64fef71db70be6
wouterhhdh <wouter@heyheydehaas.com>
2016-11-10 15:46:26 +0100

    forgot some things
    like linking to the app.css instead of style.css and changed color into
    background-color

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -5,7 +5,7 @@
     <title>Document</title>
 
     <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-    <link rel="stylesheet" type="text/css" href="/css/style.css">
+    <link rel="stylesheet" type="text/css" href="/css/app.css">
     {{-- /css/style.css verwijst naar public/css/style.css wanneer deze online staat --}}
     @yield ('header')
 </head>
