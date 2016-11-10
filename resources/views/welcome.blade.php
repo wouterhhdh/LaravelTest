2aad5e3f94ecb68f549eba1c7aae5291cf30c973
wouterhhdh <wouter@heyheydehaas.com>
2016-11-10 14:54:56 +0100

    about.blade.php and welcome.blade.php
    beide maken gebruik van de layout in layout.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,6 +3,6 @@
 
 @section('content') 
 {{-- alles wat hier staat is dus 'content' en zal worden geplaatst binnen layout.blade.php op de plek waar 'content' wordt genoemd --}}
-
+    <h1>welcom page</h1>
 @stop
  
