5671d1a6fa1634f6df120f9e41e2fb19c2838fa4
wouterhhdh <wouter@heyheydehaas.com>
2016-11-10 14:50:48 +0100

    made layout.blade.php
    dit document wordt hergebruikt en bevat een plek waar wisselende
    content wordt geplaatst (@yield)

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css"> 
+    <!-- In dit document plaats ik elementen die ik binnen eenzelfde layout wil gebruiken. Alle pagina's die gekoppeld worden aan dit document krijgen dus deze zelfde elementen mee -->
+</head>
+<body>
+
+    @yield('content') <!-- alle secties die we 'content' genoemd hebben wil ik hier plaatsen -->
+    
+</body>
+</html>
