3e88735aa8f00c2e8e089943221f4af01635b9f0
wouterhhdh <wouter@heyheydehaas.com>
2016-11-10 16:39:44 +0100

    *ignore missing ;

--- app/Http/Controllers/CardsController.php
+++ app/Http/Controllers/CardsController.php
@@ -6,9 +6,8 @@ use Illuminate\Http\Request;
 
 class CardsController extends Controller
 {
-    public function index(){
-
-    	return view('cards.index')
-
+    public function index()
+    {
+    	return view('cards.index');
     }
-}
+};
